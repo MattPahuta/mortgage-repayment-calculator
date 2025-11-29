6ecf1cdddae3228b175279720f42eb9b74877291
Matt Pahuta <mhpahuta@gmail.com>
2025-11-29 15:56:12 -0800

    Add input styles and props, primary button styles

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,22 +1,28 @@
-// Props: type, label text, accent/spacer content, accent/spacer orientation (start/end)
-// FormInput with accent div for accent characters and years
-function FormInput() {
+// Props: type, id, label text, accent label content, input order (1 or 0), supplemental (hidden) label content
+// handle order more gracefully - 1 on the Form component, translate to 
+
+function FormInput({type, id, label, accentContent, accentLabel, order = ''}) {
   return (
-    <div className="">
-      <label htmlFor="mortgageAmount" className="">
-        Mortgage Amount
-        {/* Add prop to change flex property - row-reverse? */}
-        <div className="group mt-2 flex items-center rounded-sm border border-slate-500 overflow-hidden hover:border-amber-500 transition">
-          <div className="bg-sky-100 px-3.5 py-2 text-lg font-bold">$</div>
+    <div className="grid gap-2.5">
+      <label htmlFor={id} className="">
+        {label} 
+        <span className="sr-only">{accentLabel}</span>
+        <div className="group mt-2.5 flex items-center rounded-sm border border-slate-400 overflow-hidden shadow-sm hover:border-slate-900 transition has-focus:border-yellow-300">
           <input
-            type="text"
-            id="mortgageAmount"
-            className="w-full mx-2 text-slate-900 font-semibold outline-0"
+            type={type}
+            id={id}
+            aria-describedby={`${id}-error`}
+            className={`${order} peer w-full mx-2 text-slate-900 font-semibold outline-0 group-hover:cursor-pointer`}
           />
+          <div className="bg-sky-100 px-3.5 py-2 text-lg font-bold peer-focus:bg-yellow-300">
+            {accentContent}
+          </div>
         </div>
       </label>
       {/* error notification */}
-      <div></div>
+      <div id={`${id}-error`} className="hidden" aria-live="assertive">
+        <p className="text-red-600">This field is required</p>
+      </div>
     </div>
   );
 }
--- src/components/MortgageForm.jsx
+++ src/components/MortgageForm.jsx
@@ -1,41 +1,48 @@
 import FormInput from './FormInput';
+import { Calculator } from 'lucide-react';
 
 function MortgageForm() {
   return (
-    <form noValidate className="py-8 px-6 space-y-6 bg-white text-slate-700">
+    <form
+      noValidate
+      className="py-8 px-6 space-y-6 bg-white text-slate-600">
       <header className="flex flex-col items-start">
-        <h1 className="mb-2 text-2xl font-bold text-slate-900">Mortgage Calculator</h1>
-        <button className="font-medium cursor-pointer border-b border-slate-700">Clear All</button>
+        <h1 className="mb-2 text-2xl font-bold text-slate-900">
+          Mortgage Calculator
+        </h1>
+        <button className="text-slate-600 cursor-pointer border-b border-slate-600 hover:text-slate-900 hover:border-slate-900 focus-visible:outline-2 focus-visible:outline-offset-1  focus-visible:outline-slate-900 focus-visible:border-0">
+          Clear All
+        </button>
       </header>
       <div className="grid gap-6">
         {/* Input group */}
-        <FormInput />
-        {/* <div className="">
-          <label htmlFor="" className="">Label text
-          <div className="flex">
-            <span className=""></span>
-            <input type="text" className="w-full rounded-sm px-3 py-1.5 text-slate-900 outline-1 -outline-offset-1 outline-slate-700" />
-          </div>
-          </label>
-        </div> */}
+        <FormInput
+          type="text"
+          id="mortgageAmount"
+          label="Mortgage Amount"
+          accentContent="$"
+          accentLabel="in dollars"
+          order="order-1"
+        />
         {/* Input group */}
-        <div className="">
-          <label htmlFor="" className="">Label text
-          <div className="flex">
-            <span className=""></span>
-            <input type="text" className="bg-white" />
-          </div>
-          </label>
-        </div>
+        <FormInput
+          type="text"
+          id="mortgageTerm"
+          label="Mortgage Term"
+          accentContent="years"
+          accentLabel="in years"
+          order="order-0"
+        />
         {/* Input group */}
-        <div className="">
-          <label htmlFor="" className="">Label text
-          <div className="flex">
-            <span className=""></span>
-            <input type="text" className="bg-white" />
-          </div>
-          </label>
-        </div>
+        <FormInput
+          type="text"
+          id="interestRate"
+          label="Interest Rate"
+          accentContent="%"
+          accentLabel="percent"
+          order="order-0"
+        />
+
         {/* Radio fieldset */}
         <fieldset>
           <legend>Mortgage Type</legend>
@@ -49,9 +56,24 @@ function MortgageForm() {
           </label>
         </fieldset>
       </div>
-      <button className="">Calculate Repayments</button>
+      <button type="submit" className="w-full py-3.5 px-4 inline-flex items-center justify-center gap-2 text-lg font-bold text-slate-900 bg-lemon-lime rounded-full cursor-pointer hover:bg-lemon-lime/50 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-900 transition">
+        <svg
+          focusable="false"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24">
+          <path
+            fill="#133041"
+            d="M18.75 2.25H5.25a1.5 1.5 0 0 0-1.5 1.5v16.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V3.75a1.5 1.5 0 0 0-1.5-1.5Zm-10.5 16.5a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 18.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 15a1.125 1.125 0 1 1 0-2.25A1.125 1.125 0 0 1 12 15Zm3.75 3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm1.5-5.25a.75.75 0 0 1-.75.75h-9a.75.75 0 0 1-.75-.75V6a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 .75.75v3.75Z"
+          />
+        </svg>
+        Calculate Repayments
+      </button>
     </form>
-  )
+  );
 }
 
 export default MortgageForm;
